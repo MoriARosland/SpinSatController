7cf148b0dc4b333e76667e92e667a6a4ba3a986b
Mori Rosland MAC <moriar@stud.ntnu.no>
2025-03-26 15:31:27 +0100

    initial commit

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "raylib/raylib.h"
+#include "raylib/clay_renderer_raylib.c"
+
+#define CLAY_IMPLEMENTATION
+#include "clay.h"
+
+int main(void)
+{
+    printf("Hello, Clay and Spinsat\n");
+    return 0;
+}
---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Remove existing build directory if it exists
+rm -rf build
+
+# Create and enter build directory
+mkdir -p build
+cd build
+
+# Configure and build with CMake
+cmake -DCMAKE_BUILD_TYPE=Debug ..
+make
+
+# Check if build was successful
+if [ $? -eq 0 ]; then
+    echo -e "\nBuild successful! Running program:\n"
+    ./spin_sat_controller
+else
+    echo -e "\nBuild failed!"
+    exit 1
+fi 
