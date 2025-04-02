3c7dd2a29120759df94119f7b67d063c0cfdc84b
Mori Adrian Rosland <mori@live.no>
2025-04-02 15:05:35 +0200

    feat: build and render root layout

--- main.c
+++ main.c
@@ -1,28 +1,45 @@
 #include <stdio.h>
 #include "raylib/raylib.h"
 #include "raylib/clay_renderer_raylib.c"
+#include "error_handler.h"
 
 #define CLAY_IMPLEMENTATION
 #include "clay.h"
 
-const float screenWidth = 1024.0f;
+const float screenWidth = 1280.0f;
 const float screenHeight = 768.0f;
 
-void HandleClayErrors(Clay_ErrorData errorData)
+/// @brief Creates the layout for the UI within a Clay_BeginLayout/Clay_EndLayout boundry.
+/// @return Clay_RenderCommandArray
+Clay_RenderCommandArray CreateLayout(void)
 {
-    // See the Clay_ErrorData struct for more information
-    printf("%s", errorData.errorText.chars);
-    switch (errorData.errorType)
+    Clay_BeginLayout();
+
+    CLAY({.id = CLAY_ID("OuterContainer"), .layout = {.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0)}, .padding = {16, 16, 16, 16}, .childGap = 16}, .backgroundColor = {245, 245, 245, 255}})
     {
-        // etc
+        CLAY({.id = CLAY_ID("TopHeaderBar"), .layout = {.layoutDirection = CLAY_TOP_TO_BOTTOM, .sizing = {.width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_FIXED(64)}, .padding = {16, 16, 16, 16}, .childGap = 16}, .backgroundColor = {200, 200, 200, 255}, .cornerRadius = {16, 16, 16, 16}}) {}
+        CLAY({.id = CLAY_ID("MainBody"), .layout = {.layoutDirection = CLAY_LEFT_TO_RIGHT, .sizing = {.width = CLAY_SIZING_GROW(0), .height = CLAY_SIZING_GROW(0)}, .padding = {16, 16, 16, 16}, .childGap = 16}, .backgroundColor = {150, 150, 255, 255}}) {}
     }
+
+    return Clay_EndLayout();
+}
+
+void UpdateDrawFrame(Font *fonts)
+{
+    // Add logic for handling events here
+
+    // Build the layout
+    Clay_RenderCommandArray renderCommands = CreateLayout();
+
+    // Render the layout
+    BeginDrawing();
+    ClearBackground(RAYWHITE);
+    Clay_Raylib_Render(renderCommands, fonts);
+    EndDrawing();
 }
 
 int main(void)
 {
-    printf("Booting up...\n");
-
-    Clay_Raylib_Initialize((int)screenWidth, (int)screenHeight, "SpinSat Controller", 0);
 
     uint64_t clayMemorySize = Clay_MinMemorySize();
     Clay_Arena memoryArena = Clay_CreateArenaWithCapacityAndMemory(clayMemorySize, malloc(clayMemorySize));
@@ -32,15 +49,22 @@ int main(void)
     };
 
     Clay_Initialize(memoryArena, dimensions, (Clay_ErrorHandler){HandleClayErrors});
+    Clay_Raylib_Initialize((int)screenWidth, (int)screenHeight, "SpinSat Controller", 0);
+
+    Font fonts[1];
+    fonts[0] = GetFontDefault();
+    Clay_SetMeasureTextFunction(Raylib_MeasureText, fonts);
 
     while (!WindowShouldClose())
     {
-        /* code */
+        Clay_RenderCommandArray renderCommands = CreateLayout();
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
+        Clay_Raylib_Render(renderCommands, fonts);
         EndDrawing();
     }
 
+    Clay_Raylib_Close();
     return 0;
 }
